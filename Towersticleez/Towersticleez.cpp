b51554c4a88f95a9b7b8109eb2036102104a6b79
Brian Hebert <mrabear@gmail.com>
2013-05-09 14:43:02 -0400

    Basic Direct3D window and game loop created.

--- Towersticleez/Towersticleez.cpp
+++ Towersticleez/Towersticleez.cpp
@@ -1,6 +1,19 @@
 // include the basic windows header file
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d9.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d9.lib")
+
+// global declarations
+LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+
+// function prototypes
+void initD3D(HWND hWnd);    // sets up and initializes Direct3D
+void render_frame(void);    // renders a single frame
+void cleanD3D(void);    // closes Direct3D and releases memory
 
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd,
@@ -41,7 +54,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
                           WS_OVERLAPPEDWINDOW,    // window style
                           600,    // x-position of the window
                           300,    // y-position of the window
-                          600,    // width of the window
+                          800,    // width of the window
                           600,    // height of the window
                           NULL,    // we have no parent window, NULL
                           NULL,    // we aren't using menus, NULL
@@ -51,20 +64,36 @@ int WINAPI WinMain(HINSTANCE hInstance,
     // display the window on the screen
     ShowWindow(hWnd, nCmdShow);
 
+	// set up and initialize Direct3D
+    initD3D(hWnd);
+
     // enter the main loop:
 
     // this struct holds Windows event messages
     MSG msg;
 
-    // wait for the next message in the queue, store the result in 'msg'
-    while(GetMessage(&msg, NULL, 0, 0))
+    // Enter the infinite message loop
+    while(TRUE)
     {
-        // translate keystroke messages into the right format
-        TranslateMessage(&msg);
+        // Check to see if any messages are waiting in the queue
+        while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            // translate keystroke messages into the right format
+            TranslateMessage(&msg);
 
-        // send the message to the WindowProc function
-        DispatchMessage(&msg);
+            // send the message to the WindowProc function
+            DispatchMessage(&msg);
+        }
+
+        // If the message is WM_QUIT, exit the while loop
+        if(msg.message == WM_QUIT)
+            break;
+
+		render_frame();
     }
+
+	// clean up DirectX and COM
+    cleanD3D();
 
     // return this part of the WM_QUIT message to Windows
     return msg.wParam;
@@ -87,4 +116,47 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 
     // Handle any messages the switch statement didn't
     return DefWindowProc (hWnd, message, wParam, lParam);
+}
+
+// this function initializes and prepares Direct3D for use
+void initD3D(HWND hWnd)
+{
+    d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
+
+    D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+
+    ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
+    d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
+    d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
+
+    // create a device class using this information and information from the d3dpp stuct
+    d3d->CreateDevice(D3DADAPTER_DEFAULT,
+                      D3DDEVTYPE_HAL,
+                      hWnd,
+                      D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+                      &d3dpp,
+                      &d3ddev);
+}
+
+// this is the function used to render a single frame
+void render_frame(void)
+{
+    // clear the window to a deep blue
+    d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+
+    d3ddev->BeginScene();    // begins the 3D scene
+
+    // do 3D rendering on the back buffer here
+
+    d3ddev->EndScene();    // ends the 3D scene
+
+    d3ddev->Present(NULL, NULL, NULL, NULL);    // displays the created frame
+}
+
+// this is the function that cleans up Direct3D and COM
+void cleanD3D(void)
+{
+    d3ddev->Release();    // close and release the 3D device
+    d3d->Release();    // close and release Direct3D
 }
